1c127808bc7f16196a4114232a876f2904a733c7
Muhammad Ilham <muhilham@go-jek.com>
2016-07-19 16:54:38 +0700

    fix error from last commit

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello world</h1>
-      <b>This would cause error in (check developer tools)</b>
+      <div>
+        <h1>Hello world</h1>
+        <b>This fix from last commit</b>
+      </div>
     )
   }
 }
