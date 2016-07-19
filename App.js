df0c04ed6b236291e931f587ca1e62a0f1fd5f77
Muhammad Ilham <muhilham@go-jek.com>
2016-07-19 16:53:23 +0700

    you cannot return two element in a single return (THIS WOULD CAUSE AN ERROR)

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 class App extends React.Component {
   render() {
-    return <h1>Hello world</h1>
+    return (
+      <h1>Hello world</h1>
+      <b>This would cause error in (check developer tools)</b>
+    )
   }
 }
 export default App;
