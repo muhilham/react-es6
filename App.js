4449ae837d42d216b39fc783ef0d9630782c2539
Muhammad Ilham <muhilham@go-jek.com>
2016-07-22 15:45:18 +0700

    increment value function

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 0
+    };
+
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({
+      val: this.state.val +1
+    });
+  }
+
   render() {
-    let txt = this.props.txt;
     return (
-      <div>
-        <h1>Hello world</h1>
-        <b>{txt}</b>
-      </div>
+      <button onClick={this.update}>{this.state.val}</button>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'Fun YO mama'
-}
-
 ReactDOM.render(
-  <App cat={69}/>,
+  <App/>,
   document.getElementById('app')
 );
 export default App;
