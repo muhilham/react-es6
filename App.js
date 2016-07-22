7407df863b2927b209c6522e8b2f3facfcf37a3b
Muhammad Ilham <muhilham@go-jek.com>
2016-07-22 17:39:08 +0700

    what happen when you unmount some shit

--- App.js
+++ App.js
@@ -27,10 +27,39 @@ class App extends React.Component {
   componentDidMount() {
     console.log('this only trigger once after rendering');
   }
+  componentWillUnmount() {
+    console.log('this shit trigger once the component being unmount');
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+  mount() {
+    ReactDOM.render(
+      <App/>,
+      document.getElementById('a')
+    );
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+  render() {
+    return (
+      <section>
+        <button onClick={this.mount}>Mount shit</button>
+        <button onClick={this.unmount}>Unmount shit</button>
+        <span id="a"></span>
+      </section>
+    );
+  }
 }
 
 ReactDOM.render(
-  <App/>,
+  <Wrapper/>,
   document.getElementById('app')
 );
-export default App;
+export default Wrapper;
