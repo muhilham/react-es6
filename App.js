494036c18ffb1ce3b5dcd180413c3ab43a4b00e6
Muhammad Ilham <muhilham@go-jek.com>
2016-07-22 18:09:09 +0700

    mounting usage

--- App.js
+++ App.js
@@ -1,28 +1,68 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      val: 0
+    };
+
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({
+      val: this.state.val +1
+    });
+  }
+  componentWillMount() {
+    this.setState({
+      multiplier: 2
+    });
+  }
   render() {
-    let txt = this.props.txt;
+    console.log('this will always keep firing');
     return (
-      <div>
-        <h1>Hello world</h1>
-        <b>{txt}</b>
-      </div>
+      <button onClick={this.update}>{this.state.val * this.state.val}</button>
+    );
+  }
+  componentDidMount() {
+    this.inc = setInterval(this.update, 500) ;
+  }
+  componentWillUnmount() {
+    console.log('this shit trigger once the component being unmount');
+    clearInterval(this.inc);
+  }
+}
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+  mount() {
+    ReactDOM.render(
+      <App/>,
+      document.getElementById('a')
+    );
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+  render() {
+    return (
+      <section>
+        <button onClick={this.mount}>Mount shit</button>
+        <button onClick={this.unmount}>Unmount shit</button>
+        <span id="a"></span>
+      </section>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'Fun YO mama'
-}
-
 ReactDOM.render(
-  <App cat={69}/>,
+  <Wrapper/>,
   document.getElementById('app')
 );
-export default App;
+export default Wrapper;
