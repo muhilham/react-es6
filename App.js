8400a149e213794fff79f6dbd049192114c8068a
Muhammad Ilham <muhilham@go-jek.com>
2016-07-25 14:28:34 +0700

    which method triggered first

--- App.js
+++ App.js
@@ -3,38 +3,46 @@ import React from 'react';
 
 class App extends React.Component {
   constructor(props) {
+    console.log('constructor');
     super(props);
     this.update = this.update.bind(this);
     this.state = {
       increase : false,
       currentProps: null,
-      nextProps: null
+      nextProps: null,
+      isRendered: false
     };
   }
 
   update() {
+    console.log('update');
     ReactDOM.render(
       <App val={this.props.val + 1}/>,
       document.getElementById('app')
     );
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+    let isRendered = nextProps.val % 5 === 0;
+    return isRendered;
+  }
+  
   componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps');
     this.setState({
       increase: nextProps.val > this.props.val,
       currentProps: this.props.val,
-      nextProps: nextProps.val
+      nextProps: nextProps.val,
+      isRendered: nextProps.val % 5 === 0
     });
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
   }
 
   render() {
     console.log({
       now: this.state.currentProps,
-      nextProps: this.state.nextProps
+      nextProps: this.state.nextProps,
+      isRendered: this.state.isRendered
     },this.state.increase);
     return (
       <button onClick={this.update}> {this.props.val} </button>
