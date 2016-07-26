d1c1a8788c73b3dc62cc3c270f31151ec43c9e01
Muhammad Ilham <muhilham@go-jek.com>
2016-07-26 15:45:45 +0700

    Dynamically generated component

--- App.js
+++ App.js
@@ -1,28 +1,63 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      username: '',
+      lastGistUrl: ''
+    };
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.serverRequest = $.get(this.props.source, this.serverResponse.bind(this));
+  }
+
+  serverResponse(result) {
+    console.log('serverResponse',result);
+    let lastGist = result[0];
+    this.setState({
+      data: result,
+      username: lastGist.owner.login,
+      lastGistUrl: lastGist.html_url
+    });
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    this.serverRequest.abort();
+  }
+
   render() {
-    let txt = this.props.txt;
-    return (
+    let rows = this.state.data.map( (gist, index) => {
+      return (
+        <PersonRow key={index} index={index} data={gist} />
+      );
+    });
+    return(
       <div>
-        <h1>Hello world</h1>
-        <b>{txt}</b>
+        <table>
+          <tbody>{rows}</tbody>
+        </table>
       </div>
     );
   }
 }
 
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'Fun YO mama'
-}
+const PersonRow = (props) => {
+  return (
+    <tr>
+      <td>{props.index}</td>
+      <td>{props.data.description}</td>
+      <td>{props.data.url}</td>
+    </tr>
+  );
+};
 
 ReactDOM.render(
-  <App cat={69}/>,
+  <App source="https://api.github.com/users/octocat/gists" />,
   document.getElementById('app')
 );
 export default App;
